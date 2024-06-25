f873db6933887d883068a1c80dd54d080b1b572d
Catherine Tang <aug16b@gmail.com>
2024-06-24 22:37:31 -0400

    Initial Commit

---
+++ question app/question_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  question_appApp.swift
+//  question app
+//
+//  Created by catherine on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct question_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
